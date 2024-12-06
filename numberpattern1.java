bb14375e80e242af1a1e51a702f3705052d565c0
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-06 15:15:37 +0530

    Add files via upload

---
+++ numberpattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class numberpattern1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=n;i>=1;--i)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                System.out.print(j+" ");
+                  
+            }
+            System.out.println();
+        }
+    }   
+}
