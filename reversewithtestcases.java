04ff645ee9f38095a68e50d554d35f6786cd511d
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-06 11:18:56 +0530

    Add files via upload

---
+++ reversewithtestcases.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class reversewithtestcases{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+        int n=sc.nextInt();
+        int rev=0;                                                                                
+        while(n>0)
+        {
+            int digit=n%10;
+            rev=rev*10+digit;
+            n/=10;
+            {
+                System.out.println(rev);
+            }
+        }
+    }
+    }
+}
+    
